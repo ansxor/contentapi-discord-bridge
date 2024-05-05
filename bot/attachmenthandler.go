31251e0f44e265100e961c05da5ffc4a26662e63
ansxor <darienreese@gmail.com>
2024-05-05 10:27:30 -0400

    Made GetMappedAttachment function

--- bot/attachmenthandler.go
+++ bot/attachmenthandler.go
@@ -3,9 +3,13 @@ package bot
 import (
 	"errors"
 	"io"
+	"log"
 	"net/http"
+	"path"
 	"slices"
 	"strconv"
+
+	"github.com/ansxor/contentapi-discord-bridge/contentapi"
 )
 
 // 25 MB
@@ -23,36 +27,50 @@ var ACCEPTED_MIME_TYPES = []string{
 	"image/tga",
 }
 
-func GetAttachmentStream(url string) (io.ReadCloser, error) {
+func GetAttachmentStream(url string) (io.ReadCloser, *string, error) {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	contentLength, err := strconv.Atoi(resp.Header.Get("Content-Length"))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	if contentLength > MAXIMUM_FILE_SIZE {
-		return nil, errors.New("content length is over the maximum file size of 25MB")
+		return nil, nil, errors.New("content length is over the maximum file size of 25MB")
 	}
 
 	contentType := resp.Header.Get("Content-Type")
 	if !slices.Contains(ACCEPTED_MIME_TYPES, contentType) {
-		return nil, errors.New("content type is not in the list of accepted types")
+		return nil, nil, errors.New("content type is not in the list of accepted types")
 	}
 
-	return resp.Body, nil
+	fileName := path.Base(req.URL.Path)
+	return resp.Body, &fileName, nil
 }
 
-func HandleAttachment(url string, contentapiDomain string, contentapiToken string) {
+func GetMappedAttachment(url string, contentapiDomain string, contentapiToken string) (*string, error) {
+	fileStream, fileName, err := GetAttachmentStream(url)
+	// Fallback to using the original URL if there is no way to reasonably upload to ContentAPI
+	if err != nil {
+		return &url, nil
+	}
 
+	attachmentUrl, err := contentapi.UploadFile(contentapiDomain, contentapiToken, "discord-bridge-attachments", fileStream, *fileName)
+	if err != nil {
+		log.Default().Println("Failed to upload attachment to ContentAPI")
+		log.Default().Println(err)
+		return &url, nil
+	}
+
+	return &attachmentUrl, nil
 }
--- bot/attachmenthandler_test.go
+++ bot/attachmenthandler_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestGetAttachmentStreamSupportedFileTypeAndFileSize(t *testing.T) {
-	_, err := GetAttachmentStream("https://smilebasicsource.com/contentapi/images/raw/cnvsbs-upload-1229")
+	_, _, err := GetAttachmentStream("https://smilebasicsource.com/contentapi/images/raw/cnvsbs-upload-1229")
 
 	if err != nil {
 		t.Error(err)
@@ -13,7 +13,7 @@ func TestGetAttachmentStreamSupportedFileTypeAndFileSize(t *testing.T) {
 }
 
 func TestGetAttachmentStreamIncorrectFileType(t *testing.T) {
-	_, err := GetAttachmentStream("https://smilebasicsource.com")
+	_, _, err := GetAttachmentStream("https://smilebasicsource.com")
 
 	if err == nil {
 		t.Error("Getting attachment stream is supposed to fail when given the incorrect file type.")
@@ -21,7 +21,7 @@ func TestGetAttachmentStreamIncorrectFileType(t *testing.T) {
 }
 
 func TestGetAttachmentStreamIncorrectFileSize(t *testing.T) {
-	_, err := GetAttachmentStream("https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73751/world.topo.bathy.200407.3x21600x21600.C1.png")
+	_, _, err := GetAttachmentStream("https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73751/world.topo.bathy.200407.3x21600x21600.C1.png")
 
 	if err == nil {
 		t.Error("Getting attachment stream is supposed to fail when file size is above 25MB.")
